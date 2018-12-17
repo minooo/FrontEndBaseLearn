0db16085d2c6b75e6a88fc0c87134fd2a110e443
minooo <13603983223@163.com>
2018-12-17 11:42:47 +0800

    base

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const handle = require('./src/01-form')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    handle(req, res)
+})
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
